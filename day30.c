87879eaeeb2176fe454e93528851bc2970a0019f
Himanshi Chauhan <himanshi@gmail.com>
2026-07-26 23:07:32 +0530

    30commit

---
+++ day30.c
@@ -0,0 +1,94 @@
+/*Problem: Polynomial Using Linked List - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n (number of terms)
+- Next n lines: two integers (coefficient and exponent)
+
+Output:
+- Print polynomial in standard form, e.g., 10x^4 + 20x^3 + 30x^2 + 40x + 50
+
+Example:
+Input:
+5
+10 4
+20 3
+30 2
+40 1
+50 0
+
+Output:
+10x^4 + 20x^3 + 30x^2 + 40x + 50
+
+Explanation:
+Each node stores coefficient and exponent. Traverse nodes to print polynomial in decreasing exponent order.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for a polynomial term
+struct Node {
+    int coeff;
+    int exp;
+    struct Node *next;
+};
+
+// Function to create a new node
+struct Node* createNode(int coeff, int exp) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->coeff = coeff;
+    newNode->exp = exp;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the end
+void insert(struct Node **head, int coeff, int exp) {
+    struct Node *newNode = createNode(coeff, exp);
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node *temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+}
+
+// Function to display the polynomial
+void display(struct Node *head) {
+    struct Node *temp = head;
+
+    while (temp != NULL) {
+        if (temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if (temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+
+        if (temp->next != NULL)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+// Main function
+int main() {
+    int n, coeff, exp;
+    struct Node *head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &coeff, &exp);
+        insert(&head, coeff, exp);
+    }
+
+    display(head);
+
+    return 0;
+} 
